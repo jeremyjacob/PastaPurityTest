31d49b2b5f525feffaf9a7ea58b26f4f0354d217
Jeremy Jacob <github@jeremyjacob.dev>
2020-05-18 23:12:48 -0700

    upload

---
+++ scripts.js
@@ -0,0 +1,13 @@
+document.querySelector('#submit').addEventListener('click', event => {
+    let checked = 0;
+    document.querySelectorAll('input').forEach(element => {
+        if (element.checked) {
+            checked++;
+        }
+    });
+    console.log(checked);
+    document.querySelector('#content').style.display = 'none';
+    document.querySelector('#instruction').style.display = 'none';
+    document.querySelector('#result').style.display = 'block';
+    document.querySelector('#score').innerText = 100 - checked;
+});
